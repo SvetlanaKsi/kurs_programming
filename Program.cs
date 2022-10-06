0d737014244077f608189a7de46e1eaf3ba55e64
sveta k <skasitsina@yandex.ru>
2022-10-07 00:44:55 +0300

    решила задачу 2

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите ваше имя: ");
-string name = Console.ReadLine();
-Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine());
-Console.WriteLine($"Квадрат {number} равен {number*number}");
-Console.WriteLine("Ваше имя " + name);
+﻿/*Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1>number2)
+    Console.WriteLine($"Первое число {number1} большее, а второе число {number2} меньшее");
+    else
+    Console.WriteLine($"Второе число {number2} большее, а первое число {number1} меньшее");
+*/    
