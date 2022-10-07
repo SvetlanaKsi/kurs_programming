3e9f4092eea57eddef4f38afef956377510cc869
sveta k <skasitsina@yandex.ru>
2022-10-07 12:07:09 +0300

    решила вторую задачу

--- Program.cs
+++ Program.cs
@@ -8,3 +8,15 @@ if (number1>number2)
     else
     Console.WriteLine($"Второе число {number2} большее, а первое число {number1} меньшее");
 */    
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+int max = number1;
+if (number2>max) 
+    max = number2;
+if (number3>max) 
+    max = number3;
+Console.WriteLine("Масксимальное число " + max);
