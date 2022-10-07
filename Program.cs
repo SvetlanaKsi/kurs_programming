5bfc99b6b74f745639fdee20bcde846d75afbb9c
sveta k <skasitsina@yandex.ru>
2022-10-07 17:58:07 +0300

    решения не вышло

--- Program.cs
+++ Program.cs
@@ -33,3 +33,15 @@ Console.WriteLine("Число является четным");
 else
 Console.WriteLine("Число является нечетным");
 */
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int i = 2;
+while (i<=N)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i + ", ");
+        i++;
+    }
+}  
