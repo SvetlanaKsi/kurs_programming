af40ef92448e9a688494aa5cab33814a4b986681
sveta k <skasitsina@yandex.ru>
2022-12-02 19:26:23 +0300

    9 семинар

--- Program.cs
+++ Program.cs
@@ -472,7 +472,7 @@ void PrintArray(int[,] array)
     }
 }
 
-*/
+
 
 
 int InputInt(string output)
@@ -531,3 +531,66 @@ PrintArray(matrixB);
 Console.WriteLine();
 Console.WriteLine("Произведение матриц А*В");
 PrintArray(matrixC);
+
+Семинар 9 Задача 64
+int n = InputInt("Введите положительное число");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Ввели не положительное число");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+Семинар 9 Задача 66
+int m = InputInt("Введите M:");
+int n = InputInt("Введите N:");
+Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalSum(m, n - 1);
+}
+Семинар 9 задача 68
+Console.WriteLine("Введите два положительных числа: M и N.");
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+*/
